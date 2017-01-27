35c9947204a15b9eead68192653c16888ec0eb2d
Ander Ruiz <ander@hdivsecurity.com>
2017-01-27 08:29:29 +0100

    Annotation for secure ids

---
+++ src/main/java/com/github/hateoas/forms/action/SecureIdFor.java
@@ -0,0 +1,13 @@
+package com.github.hateoas.forms.action;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(METHOD)
+public @interface SecureIdFor {
+	Class<?> value();
+}
