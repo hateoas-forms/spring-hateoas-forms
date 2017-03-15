02b9e6190e62bf745f194e6365b1dd29ca686a42
Ander Ruiz <ander@hdivsecurity.com>
2017-03-15 16:06:39 +0100

    Changes for SDR

---
+++ src/main/java/com/github/hateoas/forms/spring/CustomizableActionParameterType.java
@@ -0,0 +1,52 @@
+package com.github.hateoas.forms.spring;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.ParameterizedType;
+
+import org.springframework.core.convert.TypeDescriptor;
+
+public class CustomizableActionParameterType implements ActionParameterType {
+
+	private final String name;
+
+	private final Class<?> type;
+
+	private final Annotation[] annotations;
+
+	public CustomizableActionParameterType(final String name, final Class<?> type, final Annotation... annotations) {
+		this.name = name;
+		this.type = type;
+		this.annotations = annotations;
+	}
+
+	@Override
+	public Annotation[] getAnnotations() {
+		return annotations;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public Class<?> getParameterType() {
+		return type;
+	}
+
+	@Override
+	public Class<?> getNestedParameterType() {
+		return getParameterType();
+	}
+
+	@Override
+	public TypeDescriptor nested(final int nestingLevel) {
+		return nestingLevel == 0 ? TypeDescriptor.valueOf(type) : null;
+	}
+
+	@Override
+	public ParameterizedType getParameterizedType() {
+		return null;
+	}
+
+}
