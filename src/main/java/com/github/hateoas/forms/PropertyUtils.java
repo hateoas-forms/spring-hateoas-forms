c43ef5129f311016b02e6bbda8e507055039c70e
Ander Ruiz <ander@hdivsecurity.com>
2017-03-15 13:59:28 +0100

    Minor change

--- src/main/java/com/github/hateoas/forms/PropertyUtils.java
+++ src/main/java/com/github/hateoas/forms/PropertyUtils.java
@@ -11,8 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Convenience methods to work with Java bean properties.
- * Created by Dietrich on 11.03.2015.
+ * Convenience methods to work with Java bean properties. Created by Dietrich on 11.03.2015.
  */
 public class PropertyUtils {
 
@@ -20,34 +19,35 @@ public class PropertyUtils {
 
 	}
 
-	public static Object getPropertyValue(Object currentCallValue, PropertyDescriptor propertyDescriptor) {
+	public static Object getPropertyValue(final Object currentCallValue, final PropertyDescriptor propertyDescriptor) {
 		Object propertyValue = null;
 		if (currentCallValue != null && propertyDescriptor.getReadMethod() != null) {
 			try {
-				propertyValue = propertyDescriptor.getReadMethod()
-						.invoke(currentCallValue);
-			} catch (Exception e) {
+				propertyValue = propertyDescriptor.getReadMethod().invoke(currentCallValue);
+			}
+			catch (Exception e) {
 				throw new RuntimeException("failed to read property from call value", e);
 			}
 		}
 		return propertyValue;
 	}
 
-	public static Map<String, PropertyDescriptor> getPropertyDescriptors(Object bean) {
+	public static Map<String, PropertyDescriptor> getPropertyDescriptors(final Object bean) {
 		try {
-			PropertyDescriptor[] propertyDescriptors = Introspector.getBeanInfo(bean.getClass())
-					.getPropertyDescriptors();
+			PropertyDescriptor[] propertyDescriptors = Introspector.getBeanInfo(bean.getClass()).getPropertyDescriptors();
 			Map<String, PropertyDescriptor> ret = new HashMap<String, PropertyDescriptor>();
 			for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
 				ret.put(propertyDescriptor.getName(), propertyDescriptor);
 			}
 			return ret;
-		} catch (IntrospectionException e) {
+		}
+		catch (IntrospectionException e) {
 			throw new RuntimeException("failed to get property descriptors of bean " + bean, e);
 		}
 	}
 
-	public static Constructor findDefaultCtor(Constructor[] constructors) {
+	@SuppressWarnings({ "rawtypes" })
+	public static Constructor findDefaultCtor(final Constructor[] constructors) {
 		// TODO duplicate on HtmlResourceMessageConverter
 		Constructor constructor = null;
 		for (Constructor ctor : constructors) {
@@ -58,7 +58,8 @@ public class PropertyUtils {
 		return constructor;
 	}
 
-	public static Constructor findJsonCreator(Constructor[] constructors, Class creatorAnnotation) {
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static Constructor findJsonCreator(final Constructor[] constructors, final Class creatorAnnotation) {
 		Constructor constructor = null;
 		for (Constructor ctor : constructors) {
 			if (AnnotationUtils.findAnnotation(ctor, creatorAnnotation) != null) {
@@ -70,7 +71,7 @@ public class PropertyUtils {
 	}
 
 	// TODO move to PropertyUtil and remove current method for propertyDescriptors, cache search results
-	public static Object getPropertyOrFieldValue(Object currentCallValue, String propertyOrFieldName) {
+	public static Object getPropertyOrFieldValue(final Object currentCallValue, final String propertyOrFieldName) {
 		if (currentCallValue == null) {
 			return null;
 		}
@@ -81,7 +82,7 @@ public class PropertyUtils {
 		return propertyValue;
 	}
 
-	public static Object getFieldValue(Object currentCallValue, String fieldName) {
+	public static Object getFieldValue(final Object currentCallValue, final String fieldName) {
 		try {
 			Class<?> beanType = currentCallValue.getClass();
 			Object propertyValue = null;
@@ -93,12 +94,13 @@ public class PropertyUtils {
 				}
 			}
 			return propertyValue;
-		} catch (Exception e) {
+		}
+		catch (Exception e) {
 			throw new RuntimeException("Failed to read field " + fieldName + " from " + currentCallValue.toString(), e);
 		}
 	}
 
-	public static Object getBeanPropertyValue(Object currentCallValue, String paramName) {
+	public static Object getBeanPropertyValue(final Object currentCallValue, final String paramName) {
 		if (currentCallValue == null) {
 			return null;
 		}
@@ -110,15 +112,16 @@ public class PropertyUtils {
 				if (paramName.equals(pd.getName())) {
 					Method readMethod = pd.getReadMethod();
 					if (readMethod != null) {
+						readMethod.setAccessible(true);
 						propertyValue = readMethod.invoke(currentCallValue);
 					}
 					break;
 				}
 			}
 			return propertyValue;
-		} catch (Exception e) {
-			throw new RuntimeException("Failed to read property " + paramName + " from " + currentCallValue.toString
-					(), e);
+		}
+		catch (Exception e) {
+			throw new RuntimeException("Failed to read property " + paramName + " from " + currentCallValue.toString(), e);
 		}
 	}
 }
